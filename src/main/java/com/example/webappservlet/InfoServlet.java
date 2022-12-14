8a8f1a601cc6b4f552a271e09797b0245e9fb51b
AVDudarenko <aleksandr.dudareko@gmail.com>
2022-12-14 11:44:36 +0300

    add info servlet

---
+++ src/main/java/com/example/webappservlet/InfoServlet.java
@@ -0,0 +1,23 @@
+package com.example.webappservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * In this servlet we send data of attributes in info.jsp. And then we show them on the page.
+ */
+@WebServlet(name = "infoServlet", value = "/info")
+public class InfoServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        request.setAttribute("name", "Sasha");
+        request.setAttribute("message", "I love Java");
+
+        getServletContext().getRequestDispatcher("/info.jsp").forward(request, response);
+    }
+}
